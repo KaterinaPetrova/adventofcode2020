1af759e8728af8d8750a960493dddb4f5e507c80
Ekaterina Petrova <91kathrin@gmail.com>
2020-12-21 15:09:26 +0300

    Add day 8

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,9 +1,27 @@
 class Day8 : Day() {
-    override fun task1(): Long {
-        TODO("Not yet implemented")
+    override fun task1() = execute().acc
+
+    override fun task2() = execute().history.map { execute(corruptedLine = it) }.find { it.terminated }!!.acc
+
+    private fun execute(commands: List<String> = inputLines, corruptedLine: Int? = null): ProgramExecutionResult {
+        var acc = 0
+        var lineIndex = 0
+        val history = mutableListOf<Int>()
+        while (lineIndex < commands.size) {
+            if (history.contains(lineIndex)) break
+            history.add(lineIndex)
+            val command = commands[lineIndex].split(" ")
+            val operator = command.first()
+            val argument = command.last().toInt()
+            val lineIsCorrupted = lineIndex == corruptedLine
+            when {
+                operator == "acc" -> acc += argument.also { lineIndex++ }
+                (operator == "jmp" && !lineIsCorrupted) || (operator == "nop" && lineIsCorrupted) -> lineIndex += argument
+                (operator == "nop" && !lineIsCorrupted) || (operator == "jmp" && lineIsCorrupted) -> lineIndex++
+            }
+        }
+        return ProgramExecutionResult(acc.toLong(), history, lineIndex == commands.size)
     }
 
-    override fun task2(): Long {
-        TODO("Not yet implemented")
-    }
+    class ProgramExecutionResult(val acc: Long, val history: List<Int>, val terminated: Boolean)
 }
